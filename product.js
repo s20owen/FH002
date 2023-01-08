1339a37f0c193c2be14ebe5a725c3ab0e5768782
s20owen <Scott.owen.19l@gmail.com>
2023-01-08 18:24:54 -0500

    added dynamic product selection

---
+++ product.js
@@ -0,0 +1,47 @@
+// Navigation
+const slidingNav = document.getElementById("Sidenav");
+
+function openNav(){
+    slidingNav.style.width = "100%";
+}
+
+function closeNav(){
+    slidingNav.style.width = "0";
+}
+
+const close = document.querySelectorAll(".close");
+close.forEach(element => {
+    element.addEventListener('click', () => {
+        closeNav();
+    });
+});
+
+
+const cont = document.getElementById("product-container");
+// content from shop page
+function get () {
+    // (A) GET THE PARAMETERS
+    let params = new URLSearchParams(window.location.search),
+       product = params.get("product"),
+       details = JSON.parse(params.get("details"));
+  
+    // (B) IT WORKS!
+    console.log(product);  // Foo Bar
+    cont.innerHTML += 
+        `<div class="left"> 
+            <img src="${product}"">
+        </div>
+
+        <div class="right">
+            <h1>${details[0]}</h1>
+            <p>${details[1]}</p>
+            <p>${details[2]}</p>
+            
+            <input class="stepper" type="number" id="quantity" name="quantity" step="1" value="1">
+            <p>quantity</p>
+            <span>ADD TO CART</span>
+        </div>
+    `;
+}
+
+get();
---
+++ shop.js
@@ -0,0 +1,39 @@
+
+// Navigation
+const slidingNav = document.getElementById("Sidenav");
+
+function openNav(){
+    slidingNav.style.width = "100%";
+}
+
+function closeNav(){
+    slidingNav.style.width = "0";
+}
+
+const close = document.querySelectorAll(".close");
+close.forEach(element => {
+    element.addEventListener('click', () => {
+        closeNav();
+    });
+});
+
+// pass data
+document.querySelectorAll('.content').forEach(item => {
+    item.addEventListener('click', event => {
+        
+        const string = event.target.src;
+        const result = string.split("/");
+        const path = result[3] + '/' + result[4];
+
+        const info = [item.dataset.name, item.dataset.details, item.dataset.price];
+
+        let product = "../" + path;
+
+        let params = new URLSearchParams();
+        params.append("product", product);
+        params.append("details", JSON.stringify(info));
+
+        let url = "product.html?" + params.toString();
+        location.href = url;
+    });
+});
